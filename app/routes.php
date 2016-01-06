9e4eafa0830cd8d69970d18177d1c474d049aa13
Ben Batschelet <ben@codeup.com>
2016-01-06 15:56:58 -0600

    Views for portfolio & resume. Also a really dirty solution for rolling the dice

--- app/routes.php
+++ app/routes.php
@@ -34,12 +34,12 @@ Route::get('/', function()
 
 Route::get('/resume', function()
 {
-    return "This is my resume";
+    return View::make('resume');
 });
 
 Route::get('/portfolio', function()
 {
-    return "This is my portfolio";
+    return View::make('portfolio');
 });
 
 
