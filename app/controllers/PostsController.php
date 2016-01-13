1804c210bd5ddb4061244caed5c5edeafa93fd00
ryanorsinger <ryan.orsinger@gmail.com>
2016-01-13 13:06:30 -0600

    Add errors

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -25,34 +25,6 @@ class PostsController extends \BaseController {
 	}
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-
-		// create the validator
-	    $validator = Validator::make(Input::all(), Post::$rules);
-
-	    // attempt validation
-	    if ($validator->fails()) {
-			return Redirect::back()->withInput()->withErrors($validator);
-		} else {
-			$post = new Post();
-			$post->title = Input::get('title');
-			$post->body = Input::get('body');
-
-			$result = $post->save();
-
-			if($result) {
-				return "Your post was saved!";
-			} else {
-				return Redirect::back()->withInput();
-			}
-		}
-	}
-
 
 	/**
 	 * Display the specified resource.
@@ -63,6 +35,11 @@ class PostsController extends \BaseController {
 	public function show($id)
 	{
 		$post = Post::find($id);
+
+		if(!$post) {
+			Session::flash('errorMessage', 'This post does not exist');
+			return Redirect::action('PostsController@index');
+		}
 
 		return View::make('posts.show')->with('post', $post);
 	}
@@ -81,28 +58,46 @@ class PostsController extends \BaseController {
 	}
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
+	public function store()
+	{
+		$post = new Post();
+		return $this->validateAndSave($post);
+	}
+
 	public function update($id)
 	{
-		return 'update posts';
-		//
+		$post = Post::find($id);
+		return $this->validateAndSave($post);
+	}
+	protected function validateAndSave($post)
+	{
+		// create the validator
+	    $validator = Validator::make(Input::all(), Post::$rules);
+
+	    // attempt validation
+	    if ($validator->fails()) {
+			return Redirect::back()->withInput()->withErrors($validator);
+		} else {
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+
+			$result = $post->save();
+
+			if($result) {
+				return Redirect::action('PostsController@show', $post->id);
+			} else {
+				return Redirect::back()->withInput();
+			}
+		}
 	}
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
-		//
+		$post = Post::find($id);
+		$post->delete();
+
+		return Redirect::action('PostsController@index');
 	}
 
 	public function showAuthorPosts($username)
@@ -114,4 +109,5 @@ class PostsController extends \BaseController {
 	{
 
 	}
+
 }
